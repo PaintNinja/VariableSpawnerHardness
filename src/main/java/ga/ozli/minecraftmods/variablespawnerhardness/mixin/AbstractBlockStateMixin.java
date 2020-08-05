83edaf85eecc05f853a8279d2d6140d462510ce0
Oscar Nardone <PaintNinja@users.noreply.github.com>
2020-08-05 23:11:57 +0100

    Safer method of checking difficulty, should work on servers now
    Thanks Melonslise for the suggestion!

--- src/main/java/ga/ozli/minecraftmods/variablespawnerhardness/mixin/AbstractBlockStateMixin.java
+++ src/main/java/ga/ozli/minecraftmods/variablespawnerhardness/mixin/AbstractBlockStateMixin.java
@@ -3,11 +3,11 @@ package ga.ozli.minecraftmods.variablespawnerhardness.mixin;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.storage.IWorldInfo;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
@@ -30,9 +30,8 @@ public abstract class AbstractBlockStateMixin {
      */
     @Overwrite
     public float getBlockHardness(IBlockReader worldIn, BlockPos pos) {
-        if (this.getBlock() == Blocks.SPAWNER) {
-            // todo: test if ClientWorldInfo can get out of sync with server
-            final ClientWorld.ClientWorldInfo worldInfo = Minecraft.getInstance().world.getWorldInfo();
+        if (this.getBlock() == Blocks.SPAWNER && worldIn instanceof IWorld) {
+            final IWorldInfo worldInfo = ((IWorld) worldIn).getWorld().getWorldInfo();
 
             if (worldInfo.isHardcore())
                 return 50.0F; // same as obsidian
