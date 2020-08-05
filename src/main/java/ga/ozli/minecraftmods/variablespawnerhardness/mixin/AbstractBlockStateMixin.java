6ac1f489c94112cdb73935391ae2d48a21a8a35d
Oscar Nardone <PaintNinja@users.noreply.github.com>
2020-08-05 19:04:42 +0100

    Fix mixin not applying, add difficulty-aware spawner hardness

--- src/main/java/ga/ozli/minecraftmods/variablespawnerhardness/mixin/AbstractBlockStateMixin.java
+++ src/main/java/ga/ozli/minecraftmods/variablespawnerhardness/mixin/AbstractBlockStateMixin.java
@@ -1,10 +1,12 @@
 package ga.ozli.minecraftmods.variablespawnerhardness.mixin;
 
-import ga.ozli.minecraftmods.variablespawnerhardness.VariableSpawnerHardness;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Difficulty;
 import net.minecraft.world.IBlockReader;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -24,13 +26,29 @@ public abstract class AbstractBlockStateMixin {
     /**
      * @reason Make block hardness variable for spawner blocks depending on world difficulty
      * @author Paint_Ninja
-     * Todo: Change to insert at top rather than overwriting the whole thing
+     * todo: Change to insert at top rather than overwriting the whole thing
      */
     @Overwrite
     public float getBlockHardness(IBlockReader worldIn, BlockPos pos) {
-        VariableSpawnerHardness.LOGGER.warn("this.getBlock() = " + this.getBlock()); // warn to make it obvious for dev
         if (this.getBlock() == Blocks.SPAWNER) {
-            return 50.0F;
+            // todo: test if ClientWorldInfo can get out of sync with server
+            final ClientWorld.ClientWorldInfo worldInfo = Minecraft.getInstance().world.getWorldInfo();
+
+            if (worldInfo.isHardcore())
+                return 50.0F; // same as obsidian
+
+            final Difficulty difficulty = worldInfo.getDifficulty();
+
+            switch (difficulty) {
+                case HARD:
+                    return 30.0F; // same as ancient debris
+                case NORMAL:
+                    return 22.5F; // same as enderchest
+                case EASY:
+                    return 9.0F; // same as end stone
+                default: // PEACEFUL
+                    return 5.0F; // same as vanilla spawner
+            }
         }
         return this.hardness;
     }
