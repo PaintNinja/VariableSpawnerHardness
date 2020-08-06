435ce5f6c2ef0cba2d34168b43a0c99f9cdf86db
Oscar Nardone <PaintNinja@users.noreply.github.com>
2020-08-06 19:58:53 +0100

    Fix mixin crash

--- src/main/java/ga/ozli/minecraftmods/variablespawnerhardness/mixin/AbstractBlockStateMixin.java
+++ src/main/java/ga/ozli/minecraftmods/variablespawnerhardness/mixin/AbstractBlockStateMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
+import net.minecraft.world.storage.IWorldInfo;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -25,8 +26,14 @@ public abstract class AbstractBlockStateMixin {
     @Inject(method = "getBlockHardness(Lnet/minecraft/world/IBlockReader;Lnet/minecraft/util/math/BlockPos;)F", at = @At("TAIL"), cancellable = true)
     public void getBlockHardness(IBlockReader worldIn, BlockPos pos, CallbackInfoReturnable<Float> cir) {
         if (this.getBlock() == Blocks.SPAWNER && worldIn instanceof IWorld) {
-            cir.setReturnValue(MixinCallback.onGetBlockHardness(worldIn));
+            cir.setReturnValue(onGetBlockHardness(worldIn));
         }
+    }
+
+    private static final float[] hardnessByDifficulty = {5F, 9F, 22.5F, 30F};
+    private static float onGetBlockHardness(IBlockReader worldIn) {
+        final IWorldInfo worldInfo = ((IWorld) worldIn).getWorld().getWorldInfo();
+        return worldInfo.isHardcore() ? 50F : hardnessByDifficulty[worldInfo.getDifficulty().ordinal()];
     }
 }
 
