90a48e299d7919e1efd85a71c7ddfe1f75bc5a22
Paint_Ninja <PaintNinja@users.noreply.github.com>
2020-10-09 22:49:22 +0100

    Port to fabric
    To make a jar, *don't* run `remapJar` or `jar` gradle tasks, run the `build` task instead otherwise the mixin refmap will be empty.
    
    After doing this, you may need to open the generated jars and rename the refmap file from "variablespawnerhardness-refmap.json" to "variablespawnerhardness.refmap.json" due to a bug in Fabric's build system.
    
    https://discordapp.com/channels/507304429255393322/721100785936760876/764235790741798914

--- src/main/java/ga/ozli/minecraftmods/variablespawnerhardness/VariableSpawnerHardness.java
+++ src/main/java/ga/ozli/minecraftmods/variablespawnerhardness/VariableSpawnerHardness.java
@@ -1,60 +1,15 @@
 package ga.ozli.minecraftmods.variablespawnerhardness;
 
-import net.minecraft.block.*;
-import net.minecraft.block.material.Material;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.ObjectHolder;
+import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
+public class VariableSpawnerHardness implements ModInitializer {
+	// Directly reference a log4j logger.
+	public static final Logger LOGGER = LogManager.getLogger();
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod("variablespawnerhardness")
-public final class VariableSpawnerHardness {
-    // Directly reference a log4j logger.
-    public static final Logger LOGGER = LogManager.getLogger();
-
-    public VariableSpawnerHardness() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    private void setup(final FMLCommonSetupEvent event) {
-        LOGGER.info("VariableSpawnerHardness loading");
-    }
-
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    /*
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-
-        @ObjectHolder("minecraft:spawner")
-        public static final Block SPAWNER = null;
-
-        Blocks.SPAWNER.Properties.hardness
-
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a block here
-            blockRegistryEvent.getRegistry().register(
-                    SPAWNER
-            );
-            //new SpawnerBlock(AbstractBlock.Properties.create(Material.ROCK).setRequiresTool().hardnessAndResistance(50.0F).sound(SoundType.METAL).notSolid()).setRegistryName("minecraft:spawner")
-        }
-    }*/
+	@Override
+	public void onInitialize() {
+		LOGGER.info("VariableSpawnerHardness loading");
+	}
 }
