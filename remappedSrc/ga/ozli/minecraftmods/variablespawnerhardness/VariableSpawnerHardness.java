0a4bf16c25e1cfcccf7a83f30e456057dde0521b
Oscar N <PaintNinja@users.noreply.github.com>
2020-11-04 15:50:02 +0000

    Port to 1.16.4

---
+++ remappedSrc/ga/ozli/minecraftmods/variablespawnerhardness/VariableSpawnerHardness.java
@@ -0,0 +1,15 @@
+package ga.ozli.minecraftmods.variablespawnerhardness;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class VariableSpawnerHardness implements ModInitializer {
+	// Directly reference a log4j logger.
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("VariableSpawnerHardness loading");
+	}
+}
