8eae5871f0c9002fdbb6e813b4268ecf20576b8b
Paint_Ninja <PaintNinja@users.noreply.github.com>
2022-03-08 10:17:05 +0000

    Port to 1.18.2

--- src/main/java/ga/ozli/minecraftmods/variablespawnerhardness/VariableSpawnerHardness.java
+++ src/main/java/ga/ozli/minecraftmods/variablespawnerhardness/VariableSpawnerHardness.java
@@ -1,17 +1,17 @@
 package ga.ozli.minecraftmods.variablespawnerhardness;
 
+import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("variablespawnerhardness")
 public final class VariableSpawnerHardness {
-    // Directly reference a log4j logger.
-    public static final Logger LOGGER = LogManager.getLogger();
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
 
     public VariableSpawnerHardness() {
         // Register the setup method for modloading
