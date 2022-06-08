643cb888166d332cba6e170cf3edc1e52e7f3001
Paint_Ninja <PaintNinja@users.noreply.github.com>
2022-06-08 12:26:12 +0100

    Port to 1.19, minor code cleanup

--- src/main/java/ga/ozli/minecraftmods/variablespawnerhardness/VariableSpawnerHardness.java
+++ src/main/java/ga/ozli/minecraftmods/variablespawnerhardness/VariableSpawnerHardness.java
@@ -1,8 +1,6 @@
 package ga.ozli.minecraftmods.variablespawnerhardness;
 
 import com.mojang.logging.LogUtils;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -12,26 +10,21 @@ import org.slf4j.Logger;
 
 import static ga.ozli.minecraftmods.variablespawnerhardness.VariableSpawnerHardness.MOD_ID;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(MOD_ID)
 public final class VariableSpawnerHardness {
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     static final String MOD_ID = "variablespawnerhardness";
 
     public VariableSpawnerHardness() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // Register the commonSetup method for mod loading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
 
         // Setup and register the config
         Config.init();
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.configSpec);
-
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event) {
+    private void commonSetup(final FMLCommonSetupEvent event) {
         LOGGER.info("VariableSpawnerHardness starting");
     }
 }
