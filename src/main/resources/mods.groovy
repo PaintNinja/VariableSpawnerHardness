f9bf6dd7a28314d4b5397ae0b00f8e28ea1e92af
Paint_Ninja <PaintNinja@users.noreply.github.com>
2024-06-06 18:53:58 +0100

    Port to Forge 1.20.6

--- src/main/resources/mods.groovy
+++ src/main/resources/mods.groovy
@@ -1,6 +1,6 @@
 ModsDotGroovy.make {
     modLoader = 'javafml'
-    loaderVersion = '[49,)'
+    loaderVersion = '[50,)'
 
     license = 'MIT'
     issueTrackerUrl = 'https://github.com/PaintNinja/VariableSpawnerHardness/issues'
@@ -8,7 +8,7 @@ ModsDotGroovy.make {
     mod {
         modId = 'variablespawnerhardness'
         displayName = 'Variable Spawner Hardness'
-        version = '1.20.4-1.4.1'
+        version = '1.20.6-1.4.1'
         author = 'Paint_Ninja'
 
         displayUrl = 'https://www.curseforge.com/minecraft/mc-mods/variable-spawner-hardness'
@@ -32,7 +32,7 @@ ModsDotGroovy.make {
         credits = 'Thanks to Commoble and IItemstack on the MMD discord for helping make this possible. Thanks to LexManos from the Forge team for helping make this work without any mixins by using more of the Forge API.'
 
         dependencies {
-            forge = '>=49.0.0'
+            forge = '>=50.1.0'
             minecraft = this.minecraftVersionRange
         }
     }
