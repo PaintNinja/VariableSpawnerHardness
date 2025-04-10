ea8f5ede1de92392ebd93ae902c04f4fd16819f4
Paint_Ninja <PaintNinja@users.noreply.github.com>
2025-04-10 13:20:15 +0100

    Various performance improvements, drop support for MC 1.21.0
    Backport of 1.4.5, 1.4.4 and 1.4.3 to the MC 1.21.1 branch

--- src/main/java/ga/ozli/minecraftmods/variablespawnerhardness/Config.java
+++ src/main/java/ga/ozli/minecraftmods/variablespawnerhardness/Config.java
@@ -3,16 +3,20 @@ package ga.ozli.minecraftmods.variablespawnerhardness;
 import net.minecraftforge.common.ForgeConfigSpec;
 
 final class Config {
-    static final ForgeConfigSpec configSpec;
+    private Config() {}
 
-    static final ForgeConfigSpec.FloatValue
+    static final ForgeConfigSpec CONFIG_SPEC;
+
+    private static final ForgeConfigSpec.FloatValue
             peaceful, easy, medium, hard, hardcore;
 
     private static final class LazyInit {
         private LazyInit() {}
 
+        private static final float PEACEFUL = peaceful.get();
+
         private static final float[] HARDNESS_BY_DIFFICULTY = new float[] {
-                peaceful.get(),
+                PEACEFUL,
                 easy.get(),
                 medium.get(),
                 hard.get(),
@@ -24,14 +28,18 @@ final class Config {
         final ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
 
         builder.push("Hardnesses");
-            peaceful = builder.defineInRange("Peaceful", 5F, 5F, 50F);
-            easy = builder.defineInRange("Easy", 9F, 5F, 50F);
-            medium = builder.defineInRange("Medium", 22.5F, 5F, 50F);
-            hard = builder.defineInRange("Hard", 30F, 5F, 50F);
-            hardcore = builder.defineInRange("Hardcore", 50F, 5F, 50F);
+        peaceful = builder.defineInRange("Peaceful", 5F, 5F, 50F);
+        easy = builder.defineInRange("Easy", 9F, 5F, 50F);
+        medium = builder.defineInRange("Medium", 22.5F, 5F, 50F);
+        hard = builder.defineInRange("Hard", 30F, 5F, 50F);
+        hardcore = builder.defineInRange("Hardcore", 50F, 5F, 50F);
         builder.pop();
 
-        configSpec = builder.build();
+        CONFIG_SPEC = builder.build();
+    }
+
+    static float getPeaceful() {
+        return LazyInit.PEACEFUL;
     }
 
     static float[] getHardnessByDifficulty() {
