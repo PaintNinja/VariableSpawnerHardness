1394ecf3db1c66e4f1b48ed38940a3e2239696cb
Paint_Ninja <PaintNinja@users.noreply.github.com>
2025-04-10 13:20:15 +0100

    Performance improvements

--- src/main/java/ga/ozli/minecraftmods/variablespawnerhardness/VariableSpawnerHardness.java
+++ src/main/java/ga/ozli/minecraftmods/variablespawnerhardness/VariableSpawnerHardness.java
@@ -1,27 +1,34 @@
 package ga.ozli.minecraftmods.variablespawnerhardness;
 
-import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.SpawnerBlock;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Mod("variablespawnerhardness")
 public final class VariableSpawnerHardness {
-    private static final Logger LOGGER = LogUtils.getLogger();
-
     public VariableSpawnerHardness(FMLJavaModLoadingContext context) {
-        // Register event listeners
-        MinecraftForge.EVENT_BUS.addListener(ForgeEvents::breakSpeed);
-        context.getModEventBus().addListener(VariableSpawnerHardness::onCommonSetup);
+        LoggerFactory.getLogger(VariableSpawnerHardness.class).info("VariableSpawnerHardness starting");
+
+        // Register event listener
+        MinecraftForge.EVENT_BUS.addListener(EventPriority.NORMAL, false, PlayerEvent.BreakSpeed.class, VariableSpawnerHardness::breakSpeed);
 
         // Setup and register the config
         context.registerConfig(ModConfig.Type.COMMON, Config.CONFIG_SPEC);
     }
 
-    static void onCommonSetup(FMLCommonSetupEvent event) {
-        LOGGER.info("VariableSpawnerHardness starting");
+    public static void breakSpeed(PlayerEvent.BreakSpeed event) {
+        if (event.getState().getBlock() instanceof SpawnerBlock)
+            event.setNewSpeed(event.getOriginalSpeed() * (Config.getPeaceful() / Config.getHardnessByDifficulty()[getDifficulty(event.getEntity().level())]));
+    }
+
+    private static int getDifficulty(Level level) {
+        if (level.getLevelData().isHardcore()) return 4;
+        else return level.getDifficulty().ordinal();
     }
 }
