6fe0f266f8ca2d632d78abfc6542d81d3d280f06
Paint_Ninja <PaintNinja@users.noreply.github.com>
2025-04-10 13:30:03 +0100

    Fix issue with built jar

--- src/main/resources/mods.groovy
+++ src/main/resources/mods.groovy
@@ -8,7 +8,7 @@ ModsDotGroovy.make {
     mod {
         modId = 'variablespawnerhardness'
         displayName = 'Variable Spawner Hardness'
-        version = '1.21.5-1.4.4'
+        version = '1.21.5-1.4.5'
         author = 'Paint_Ninja'
 
         displayUrl = 'https://www.curseforge.com/minecraft/mc-mods/variable-spawner-hardness'
