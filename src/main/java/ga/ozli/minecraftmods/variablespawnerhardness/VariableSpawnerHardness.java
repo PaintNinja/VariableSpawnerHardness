8f189e740e80ee668e87c53c0a0aa5702dd07434
Paint_Ninja <PaintNinja@users.noreply.github.com>
2025-05-06 13:32:07 +0100

    Various performance improvements
    Backport of 1.4.5, 1.4.4 and 1.4.3 to the MC 1.20.4 branch. Requires Forge 49.2.0 or newer

--- src/main/java/ga/ozli/minecraftmods/variablespawnerhardness/VariableSpawnerHardness.java
+++ src/main/java/ga/ozli/minecraftmods/variablespawnerhardness/VariableSpawnerHardness.java
@@ -1,31 +1,34 @@
 package ga.ozli.minecraftmods.variablespawnerhardness;
 
-import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.SpawnerBlock;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import static ga.ozli.minecraftmods.variablespawnerhardness.VariableSpawnerHardness.MOD_ID;
-
-@Mod(MOD_ID)
+@Mod("variablespawnerhardness")
 public final class VariableSpawnerHardness {
-    private static final Logger LOGGER = LogUtils.getLogger();
-    static final String MOD_ID = "variablespawnerhardness";
+    public VariableSpawnerHardness(FMLJavaModLoadingContext context) {
+        LoggerFactory.getLogger(VariableSpawnerHardness.class).info("VariableSpawnerHardness starting");
 
-    public VariableSpawnerHardness() {
-        // Register event listeners
-        MinecraftForge.EVENT_BUS.register(ForgeEvents.class);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(VariableSpawnerHardness::onCommonSetup);
+        // Register event listener
+        MinecraftForge.EVENT_BUS.addListener(EventPriority.NORMAL, false, PlayerEvent.BreakSpeed.class, VariableSpawnerHardness::breakSpeed);
 
         // Setup and register the config
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.configSpec);
+        context.registerConfig(ModConfig.Type.COMMON, Config.CONFIG_SPEC);
     }
 
-    public static void onCommonSetup(final FMLCommonSetupEvent event) {
-        LOGGER.info("VariableSpawnerHardness starting");
+    public static void breakSpeed(PlayerEvent.BreakSpeed event) {
+        if (event.getState().getBlock() instanceof SpawnerBlock)
+            event.setNewSpeed(event.getOriginalSpeed() * (Config.getPeaceful() / Config.getHardnessByDifficulty()[getDifficulty(event.getEntity().level())]));
+    }
+
+    private static int getDifficulty(Level level) {
+        if (level.getLevelData().isHardcore()) return 4;
+        else return level.getDifficulty().ordinal();
     }
 }
