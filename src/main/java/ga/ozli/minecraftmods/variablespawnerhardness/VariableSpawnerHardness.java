155c99579244757be4d99afdeb0276580003282e
Paint_Ninja <PaintNinja@users.noreply.github.com>
2025-06-20 12:42:45 +0100

    1.21.6

--- src/main/java/ga/ozli/minecraftmods/variablespawnerhardness/VariableSpawnerHardness.java
+++ src/main/java/ga/ozli/minecraftmods/variablespawnerhardness/VariableSpawnerHardness.java
@@ -2,9 +2,7 @@ package ga.ozli.minecraftmods.variablespawnerhardness;
 
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.SpawnerBlock;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -16,13 +14,13 @@ public final class VariableSpawnerHardness {
         LoggerFactory.getLogger(VariableSpawnerHardness.class).info("VariableSpawnerHardness starting");
 
         // Register event listener
-        MinecraftForge.EVENT_BUS.addListener(EventPriority.NORMAL, false, PlayerEvent.BreakSpeed.class, VariableSpawnerHardness::breakSpeed);
+        PlayerEvent.BreakSpeed.BUS.addListener(VariableSpawnerHardness::breakSpeed);
 
         // Setup and register the config
         context.registerConfig(ModConfig.Type.COMMON, Config.CONFIG_SPEC);
     }
 
-    public static void breakSpeed(PlayerEvent.BreakSpeed event) {
+    private static void breakSpeed(PlayerEvent.BreakSpeed event) {
         if (event.getState().getBlock() instanceof SpawnerBlock)
             event.setNewSpeed(event.getOriginalSpeed() * (Config.getPeaceful() / Config.getHardnessByDifficulty()[getDifficulty(event.getEntity().level())]));
     }
